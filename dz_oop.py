90a7d80d1e755328b0307495e21577e5427cfaad
Artem Sargsyan <sarkiman@icloud.com>
2022-10-23 13:37:04 +0200

    Finnished hmwrk

--- dz_oop.py
+++ dz_oop.py
@@ -1,4 +1,6 @@
 class Student:
+    counter = 0
+    students = []
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -6,6 +8,9 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        Student.counter += 1
+        Student.students.append(self)
+        
     
     def rate_hw(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
@@ -18,12 +23,19 @@ class Student:
     
     def __str__(self):
         delimeter = ','
-        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.av_grade}\nКурсы в процессе изучения: {delimeter.join(self.courses_in_progress)}\nЗавершенные курсы: {delimeter.join(self.finished_courses)}'
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.av_student_grade()}\nКурсы в процессе изучения: {delimeter.join(self.courses_in_progress)}\nЗавершенные курсы: {delimeter.join(self.finished_courses)}'
         return result
     
-    def av_grade(self):
-        average_grade = sum(self.grades.values())/len(self.grades.values())
+    def av_student_grade(self):
+        sum_grade = 0
+        counter = 0
+        for i in self.grades.values():
+            for grades in i:
+                sum_grade += int(grades)
+                counter += 1
+        average_grade = sum_grade/counter
         return average_grade
+    
     
     def _lt_(self, other):
         if not isinstance(other, Student):
@@ -51,14 +63,24 @@ class Reviewer(Mentor):
         return result
 
 class Lecturer(Mentor):
-    grades = {}
+    lecturers = []
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        Lecturer.lecturers.append(self)
 
     def __str__(self):
-        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.av_grade}'
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.av_lecturer_grade()}'
         return result
 
-    def av_grade(self):
-        average_grade = sum(self.grades.values())/len(self.grades.values())
+    def av_lecturer_grade(self):
+        sum_grade = 0
+        counter = 0
+        for i in self.grades.values():
+            for grades in i:
+                sum_grade += int(grades)
+                counter += 1
+        average_grade = sum_grade/counter
         return average_grade
 
     def _lt_(self, other):
@@ -92,11 +114,34 @@ dolores_umbridge.rate_hw(drako_malfoy, 'Potions', 7)
 harry_potter.rate_hw(albus_dumbledore, 'DADA', 9)
 harry_potter.rate_hw(severus_snape, 'Potions', 3)
 
-
 drako_malfoy.rate_hw(albus_dumbledore, 'DADA', 3)
 drako_malfoy.rate_hw(severus_snape, 'Potions', 7)
 
+def all_students_average(students_list, course):
+    sum_grades = 0
+    counter = 0
+    for i in students_list:
+        stud_av = sum(i.grades[course])/len(i.grades[course])
+        counter += 1
+        sum_grades += stud_av
+    return sum_grades/counter
+
+def all_lecturers_average(lecturers_list, course):
+    sum_grades = 0
+    counter = 0
+    for i in lecturers_list:
+        if course in i.courses_attached:
+            lect_av = sum(i.grades[course])/len(i.grades[course])
+            counter += 1
+            sum_grades += lect_av
+        else:
+            continue
+    return sum_grades/counter
 
 
-print(albus_dumbledore.av_grade)
+
+
+print(all_students_average(Student.students, "DADA"))
+print(all_lecturers_average(Lecturer.lecturers, "DADA"))
+
 
